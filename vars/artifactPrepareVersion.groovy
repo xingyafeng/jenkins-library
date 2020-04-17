62e6b0a09313059611eb773e9154c0531022050d
Your Name <kevin.hudemann@sap.com>
2020-04-17 14:31:49 +0200

    Check if sshcreds is filled correctly

--- vars/artifactPrepareVersion.groovy
+++ vars/artifactPrepareVersion.groovy
@@ -12,6 +12,9 @@ void call(Map parameters = [:]) {
         [type: 'ssh', id: 'gitSshKeyCredentialsId'],
         [type: 'usernamePassword', id: 'gitHttpsCredentialsId', env: ['PIPER_username', 'PIPER_password']],
     ]
+    sshagent(['bf6f9f15-362b-4523-acc9-98ff64adc44f']) {
+        sh "echo test before pipeexecute bin call"
+    }
     withEnv(["SSH_KNOWN_HOSTS=/var/jenkins_home/.ssh/known_hosts"]) {
         piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
     }
