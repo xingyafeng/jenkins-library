fbb9842b7931bf684098ee532b18c410ebe3d55f
Your Name <kevin.hudemann@sap.com>
2020-04-17 10:00:37 +0200

    Set known_hosts env variable

--- vars/artifactPrepareVersion.groovy
+++ vars/artifactPrepareVersion.groovy
@@ -12,7 +12,7 @@ void call(Map parameters = [:]) {
         [type: 'ssh', id: 'gitSshKeyCredentialsId'],
         [type: 'usernamePassword', id: 'gitHttpsCredentialsId', env: ['PIPER_username', 'PIPER_password']],
     ]
-
-    piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
-
+    withEnv(["SSH_KNOWN_HOSTS=~/.ssh/known_hosts"]) {
+        piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
+    }
 }
