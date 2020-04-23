d77e4ccb2c4370d30b9aa124cb2f20d064d1c64a
Your Name <kevin.hudemann@sap.com>
2020-04-23 08:53:44 +0200

    test sshCreds

--- vars/artifactPrepareVersion.groovy
+++ vars/artifactPrepareVersion.groovy
@@ -13,7 +13,7 @@ void call(Map parameters = [:]) {
         [type: 'usernamePassword', id: 'gitHttpsCredentialsId', env: ['PIPER_username', 'PIPER_password']],
     ]
 
-    //withEnv(["SSH_KNOWN_HOSTS=/var/jenkins_home/.ssh/known_hosts"]) {
-    piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
-    //}
+    withEnv(["SSH_KNOWN_HOSTS=/var/jenkins_home/.ssh/known_hosts"]) {
+        piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
+    }
 }
