0a3bf86243bfa1b5fb069495017a8a82c3b3eeb6
Your Name <kevin.hudemann@sap.com>
2020-04-23 12:56:49 +0200

    test sshCreds

--- vars/artifactPrepareVersion.groovy
+++ vars/artifactPrepareVersion.groovy
@@ -14,8 +14,8 @@ void call(Map parameters = [:]) {
     ]
     sh "printenv | sort"
     String dlCacheHost = sh(returnStdout: true, script: 'echo $DL_CACHE_HOSTNAME')
-    String dlCacheNet = sh(returnStdout: true, script: 'echo $DL_CACHE_HOSTNAME')
-    withEnv(["SSH_KNOWN_HOSTS=/var/jenkins_home/.ssh/known_hosts", "DL_CACHE_HOSTNAME=$dlCacheHost", "DL_CACHE_HOSTNAME=$dlCacheNet"]) {
+    String dlCacheNet = sh(returnStdout: true, script: 'echo $DL_CACHE_NETWORK')
+    withEnv(["SSH_KNOWN_HOSTS=/var/jenkins_home/.ssh/known_hosts", "DL_CACHE_HOSTNAME=$dlCacheHost", "DL_CACHE_NETWORK=$dlCacheNet"]) {
         piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
     }
 }
