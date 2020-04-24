70931894effa12e352005b82f1d70440b8387cb1
Your Name <kevin.hudemann@sap.com>
2020-04-24 14:06:17 +0200

    test

--- vars/artifactPrepareVersion.groovy
+++ vars/artifactPrepareVersion.groovy
@@ -9,6 +9,7 @@ import static com.sap.piper.Prerequisites.checkScript
 @Field String METADATA_FILE = 'metadata/versioning.yaml'
 
 void call(Map parameters = [:]) {
+    final script = checkScript(this, parameters) ?: this
     List credentials = [
         [type: 'ssh', id: 'gitSshKeyCredentialsId'],
         [type: 'usernamePassword', id: 'gitHttpsCredentialsId', env: ['PIPER_username', 'PIPER_password']],
@@ -16,10 +17,11 @@ void call(Map parameters = [:]) {
     sh "printenv | sort"
     //String dlCacheHost = sh(returnStdout: true, script: 'echo $DL_CACHE_HOSTNAME')
     //String dlCacheNet = sh(returnStdout: true, script: 'echo $DL_CACHE_NETWORK')
-    parameters = DownloadCacheUtils.injectDownloadCacheInMavenParameters(parameters.script, parameters)
+    parameters = DownloadCacheUtils.injectDownloadCacheInMavenParameters(script, parameters)
     List knownHosts = findFiles(glob: '**/known_hosts')
+    script.println("Knownhosts")
     for (int i=0;i<knownHosts.size();i++){
-        println(knownHosts[i])
+        script.println(knownHosts[i].path)
     }
     //withEnv(["SSH_KNOWN_HOSTS=/var/jenkins_home/.ssh/known_hosts"]) {//, "DL_CACHE_HOSTNAME=$dlCacheHost", "DL_CACHE_NETWORK=$dlCacheNet"]) {
     piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
