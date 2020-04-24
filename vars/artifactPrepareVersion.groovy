db382e9cacdbb28b103721b71fc8129485723935
Your Name <kevin.hudemann@sap.com>
2020-04-24 14:00:51 +0200

    test

--- vars/artifactPrepareVersion.groovy
+++ vars/artifactPrepareVersion.groovy
@@ -17,6 +17,10 @@ void call(Map parameters = [:]) {
     //String dlCacheHost = sh(returnStdout: true, script: 'echo $DL_CACHE_HOSTNAME')
     //String dlCacheNet = sh(returnStdout: true, script: 'echo $DL_CACHE_NETWORK')
     parameters = DownloadCacheUtils.injectDownloadCacheInMavenParameters(parameters.script, parameters)
+    List knownHosts = findFiles(glob: '**/known_hosts')
+    for (int i=0;i<knownHosts.size();i++){
+        println(knownHosts[i])
+    }
     //withEnv(["SSH_KNOWN_HOSTS=/var/jenkins_home/.ssh/known_hosts"]) {//, "DL_CACHE_HOSTNAME=$dlCacheHost", "DL_CACHE_NETWORK=$dlCacheNet"]) {
     piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
     //}
