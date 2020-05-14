09ce930f34203dd6c694f7ad9c1678fc806ead2a
Your Name <kevin.hudemann@sap.com>
2020-05-14 12:59:12 +0200

    Test download cache

--- vars/mtaBuild.groovy
+++ vars/mtaBuild.groovy
@@ -1,16 +1,13 @@
 import com.sap.piper.DownloadCacheUtils
+import static com.sap.piper.Prerequisites.checkScript
 import groovy.transform.Field
 
-import static com.sap.piper.Prerequisites.checkScript
-
 @Field String STEP_NAME = getClass().getName()
-@Field String METADATA_FILE = 'metadata/versioning.yaml'
 
 void call(Map parameters = [:]) {
     final script = checkScript(this, parameters) ?: this
-    handlePipelineStepErrors (stepName: STEP_NAME, stepParameters: parameters) {
+    parameters = DownloadCacheUtils.injectDownloadCacheInMavenParameters(script, parameters)
 
-        parameters = DownloadCacheUtils.injectDownloadCacheInMavenParameters(script, parameters)
-        piperExecuteBin parameters, STEP_NAME, "metadata/${STEP_NAME}.yaml", []
-    }
+    piperExecuteBin parameters, STEP_NAME, "metadata/${STEP_NAME}.yaml", []
 }
+
