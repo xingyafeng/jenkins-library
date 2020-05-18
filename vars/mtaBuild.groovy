1a0800ec22f61f538c72602436ee4e77e353c534
Your Name <kevin.hudemann@sap.com>
2020-05-18 11:45:17 +0200

    Try with field

--- vars/mtaBuild.groovy
+++ vars/mtaBuild.groovy
@@ -3,11 +3,12 @@ import static com.sap.piper.Prerequisites.checkScript
 import groovy.transform.Field
 
 @Field String STEP_NAME = getClass().getName()
+@Field String METADATA_FILE = 'metadata/mtaBuild.yaml'
 
 void call(Map parameters = [:]) {
     final script = checkScript(this, parameters) ?: this
     parameters = DownloadCacheUtils.injectDownloadCacheInMavenParameters(script, parameters)
 
-    piperExecuteBin parameters, STEP_NAME, "metadata/${STEP_NAME}.yaml", []
+    piperExecuteBin parameters, STEP_NAME, METADATA_FILE, []
 }
 
